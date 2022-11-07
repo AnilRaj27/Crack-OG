22822d7a1a59ebcf2c752077321cf35bec47b2a9
Anil Raj <anilrajthota@gmail.com>
2022-11-08 01:17:14 +0530

    Day-6

--- 1.Basics/2.logical_thinking/1.patterns.cpp
+++ 1.Basics/2.logical_thinking/1.patterns.cpp
@@ -266,6 +266,215 @@ int main()
     // 1234554321
     for (int i = 1; i <= n; i++)
     {
+        for (int j = 1; j <= i; j++)
+        {
+            cout << j;
+        }
+        for (int k = 1; k <= 2 * n - 2 * i; k++)
+        {
+            cout << " ";
+        }
+        for (int l = i; l >= 1; l--)
+        {
+            cout << l;
+        }
+        cout << endl;
+    }
+    cout << "\n----------" << endl;
+
+    // 1
+    // 2 3
+    // 4 5 6
+    // 7 8 9 10
+    // 11 12 13 14 15
+    int count = 1;
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            cout << count << " ";
+            count++;
+        }
+        cout << endl;
+    }
+    cout << "\n----------" << endl;
+
+    // A
+    // AB
+    // ABC
+    // ABCD
+    // ABCDE
+    int charStartValue = 65;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j <= i; j++)
+        {
+            cout << char(charStartValue + j);
+        }
+        cout << endl;
+    }
+    cout << "\n----------" << endl;
+
+    // ABCDE
+    // ABCD
+    // ABC
+    // AB
+    // A
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n - i; j++)
+        {
+            cout << char(charStartValue + j);
+        }
+        cout << endl;
+    }
+    cout << "\n----------" << endl;
+
+    // A
+    // BB
+    // CCC
+    // DDDD
+    // EEEEE
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            cout << char(charStartValue - 1 + i);
+        }
+        cout << endl;
+    }
+    cout << "\n----------" << endl;
+
+    // 	   A
+    //    ABA
+    //   ABCBA
+    //  ABCDCBA
+    // ABCDEDCBA
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n - i - 1; j++)
+        {
+            cout << " ";
+        }
+        for (int k = 0; k < i + 1; k++)
+        {
+            cout << char(charStartValue + k);
+        }
+        for (int l = i; l > 0; l--)
+        {
+            cout << char(charStartValue + l - 1);
+        }
+        cout << endl;
+    }
+    cout << "\n----------" << endl;
+
+    // E
+    // DE
+    // CDE
+    // BCDE
+    // ABCDE
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            cout << char(charStartValue + n - i + j - 1);
+        }
+        cout << endl;
+    }
+    cout << "\n----------" << endl;
+
+    // **********
+    // ****  ****
+    // ***    ***
+    // **      **
+    // *        *
+    // *        *
+    // **      **
+    // ***    ***
+    // ****  ****
+    // **********
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n - i; j++)
+        {
+            cout << "*";
+        }
+        for (int k = 0; k < 2 * i; k++)
+        {
+            cout << " ";
+        }
+        for (int l = 0; l < n - i; l++)
+        {
+            cout << "*";
+        }
+        cout << endl;
+    }
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            cout << "*";
+        }
+        for (int k = 1; k <= 2 * n - 2 * i; k++)
+        {
+            cout << " ";
+        }
+        for (int l = 1; l <= i; l++)
+        {
+            cout << "*";
+        }
+        cout << endl;
+    }
+    cout << "\n----------" << endl;
+
+    // *        *
+    // **      **
+    // ***    ***
+    // ****  ****
+    // **********
+    // ****  ****
+    // ***    ***
+    // **      **
+    // *        *
+
+    cout << "\n----------" << endl;
+
+    // *****
+    // *   *
+    // *   *
+    // *   *
+    // *****
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= n; j++)
+        {
+            if ((j > 1 && j < n) && (i > 1 && i < n))
+                cout << " ";
+            else
+                cout << "*";
+        }
+        cout << endl;
+    }
+    cout << "\n----------" << endl;
+
+    // [4 4 4 4 4 4 4]
+    // [4 3 3 3 3 3 4]
+    // [4 3 2 2 2 3 4]
+    // [4 3 2 1 2 3 4]
+    // [4 3 2 2 2 3 4]
+    // [4 3 3 3 3 3 4]
+    // [4 4 4 4 4 4 4]
+    for (int i = n; i >= 1; i--)
+    {
+        for (int j = n; j >= i; j--)
+        {
+            cout << j;
+        }
+        for (int k = 1; k < i; k++)
+        {
+            cout << i;
+        }
+        cout << endl;
     }
 
     return 0;
