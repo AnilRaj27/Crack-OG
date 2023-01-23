2f4f94c462934084012128f6a833fdfe4db72670
Anil Raj <anilrajthota@gmail.com>
2023-01-23 23:47:20 +0530

    arrays

--- problems191/4.arrays_4/1.two_sum.cpp
+++ problems191/4.arrays_4/1.two_sum.cpp
@@ -1,1 +1,28 @@
-#in
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    vector<int> arr = {2, 7, 11, 15};
+    int target = 9;
+
+    map<int, int> m = {};
+    vector<int> ans = {};
+
+    for (int i = 0; i < arr.size(); i++)
+    {
+        int diff = target - arr[i];
+        if (m.find(diff) != m.end())
+        {
+            ans = {i, m[diff]};
+        }
+        else
+        {
+            m[arr[i]] = i;
+        }
+    }
+
+    cout << ans[0] << " " << ans[1] << endl;
+
+    return 0;
+}
