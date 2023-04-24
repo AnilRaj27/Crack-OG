45c41fbadaddebffe3fb51898bf2e34202e5ac13
Anil Raj <anilrajthota@gmail.com>
2023-04-24 23:51:58 +0530

    Create 1.nth_root_of_num.cpp

---
+++ problems191/11.binary_search/1.nth_root_of_num.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+double multiply(double num, int n)
+{
+    double ans = 1.0;
+    for (int i = 1; i <= n; i++)
+    {
+        ans = ans * num;
+    }
+    return ans;
+}
+
+int main()
+{
+    int n = 3, m = 27;
+
+    double low = 1, high = 27, eps = 1e-7;
+    while ((high - low) > eps)
+    {
+        double mid = (low + high) / 2.0;
+        if (multiply(mid, n) < m)
+            low = mid;
+        else
+            high = mid;
+    }
+
+    cout << low << " " << high << endl;
+    cout << pow(m, (double)(1.0 / (double)n)) << endl;
+
+    return 0;
+}
